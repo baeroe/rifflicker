8fe901bf14cdbf4c230c778b1191df470eda56e8
Rafael  Hingerl <rafael@Rafaels-MBP.fritz.box>
2023-04-08 21:35:48 +0200

    small design change

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,21 +1,21 @@
-const path = require('path');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 // Plugins
 const htmlPlugin = new HtmlWebPackPlugin({
-  favicon: './src/assets/favicon.png',
-  template: path.resolve(__dirname, 'src', 'index.html'),
+  favicon: "./src/assets/favicon.png",
+  template: path.resolve(__dirname, "src", "index.html"),
   inject: true,
-  filename: "./index.html"
+  filename: "./index.html",
 });
 const cleanWebpackPlugin = new CleanWebpackPlugin();
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
@@ -23,29 +23,24 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
+        use: ["file-loader"],
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
-  plugins: [
-    htmlPlugin,
-    cleanWebpackPlugin,
-  ],
+  plugins: [htmlPlugin, cleanWebpackPlugin],
   resolve: {
     alias: {
-      Components: path.resolve(__dirname, 'src/components'),
-      Assets: path.resolve(__dirname, 'src/assets'),
-    }
-  }
-}
+      Components: path.resolve(__dirname, "src/components"),
+      Assets: path.resolve(__dirname, "src/assets"),
+    },
+  },
+};
